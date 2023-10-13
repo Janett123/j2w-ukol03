a56e06e99b0fa03baa3d99f8175b355875ac3b20
Janett123 <masiko@centrum.cz>
2023-10-13 08:15:59 +0200

    Vytvořena entita vizitka, vytvořen seznam v controlleru vč. naplnění údajů

--- src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
@@ -1,20 +1,30 @@
 package cz.czechitas.java2webapps.ukol3.controller;
 
+import cz.czechitas.java2webapps.ukol3.entity.Vizitka;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 /**
  * Kontroler obsluhující zobrazování vizitek.
  */
 @Controller
 public class VizitkaController {
-  @GetMapping("/")
-  public String seznam() {
-    return "seznam";
-  }
+    private final List<Vizitka> seznamPracovniku = List.of(
+            new Vizitka("Kristýna Vojtěchová", "Kostitřas", "Kolodějská 7", "Praha 1O0 00", "vojtechova@kostitras.cz", "467 123 456", "www.kostitras.cz"),
+            new Vizitka("Jan Polesnik", "Kostitřas", "Kolodějská 7", "Praha 10O 00", "polesnik@kostitras.cz", "467 123 457", null),
+            new Vizitka("Petr Šipka", "Kostitřas", "Kolodějská 7", "Praha 10O 00", "sipka@kostitras.cz", "467 123 458", "www.kostitras.cz"),
+            new Vizitka("Lenka Vrubková", "Kostitřas", "Kolodějská 7", "Praha 10O 00", "vrubkova@kostitras.cz", null, "www.kostitras.cz")
+    );
 
-  @GetMapping("/detail")
-  public String detail() {
-    return "detail";
-  }
+    @GetMapping("/")
+    public String seznam() {
+        return "seznam";
+    }
+
+    @GetMapping("/detail")
+    public String detail() {
+        return "detail";
+    }
 }
